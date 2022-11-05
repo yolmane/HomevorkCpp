5a6f3f6ea93d2d38eb6823a19ea9370ec1a5e282
Сергей <78658751+yolmane@users.noreply.github.com>
2022-11-05 17:55:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 
 main()
 {
-	printf("Hello");
+	int a,b;
+	printf("a=");
+	scanf("%i",&a);
+	printf("b=");
+	scanf("%i",&b);
+	if(a>b)
+	{
+		printf("Bol'she");
+	}
+	else
+	{
+		if(a==b)
+		{
+			printf("Ravni");	
+		}
+		else
+		{
+			printf("Men'she");
+		}
+		
+	}
 }
