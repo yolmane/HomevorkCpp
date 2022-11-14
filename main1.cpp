3841ec1a56eddfc340f75d4011473652920d1162
Сергей <78658751+yolmane@users.noreply.github.com>
2022-11-15 01:14:21 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,15 +1,58 @@
 #ioclude<stdio.h>
-// ссыль на гит: https://github.com/yolmane/HomevorkCpp/tree/lesson3.5
+// ссыль на гит: https://github.com/yolmane/HomevorkCpp/tree/lesson3.6
 main()
 {
-	int i=5;
-	int sum=0;
-	while(i<15)
+	int a;
+	while(a!=7)
 	{
-		sum=sum+i;
-		printf("%i+",i);
-		i=i+1;
+		scanf("%i",&a);
+		if(a>7)
+		{
+			printf("bolshe 7 \n");
+		}
+		else 
+		{	
+			if(a<7)
+			{
+				printf("men'she 7 \n");
+			}
+		}
+		
+		//////////////////////
+		
+			if(a>10)
+		{
+			printf("bolshe 10 \n");
+		}
+		else 
+		{	
+			if(a<10)
+			{
+				printf("men'she 10 \n");
+			}
+		}
+		
+		//////////////////////
+		
+		if(a%2!=0)
+		{
+			printf("ne delitsya na 2 \n");
+		}
+		else 
+		{	
+				printf("delitsya na 2 \n");
+		}
+		
+		//////////////////////
+		
+		if(a%3!=0)
+		{
+			printf("ne delitsya na 3 \n\n");
+		}
+		else 
+		{	
+				printf("delitsya na 3 \n\n");
+		}
 	}
-	sum=sum+i;
-	printf("%i=%i",i,sum);
+	printf("u won!!!");
 }	
