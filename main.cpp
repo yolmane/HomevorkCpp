ac7d218ea78473e15c7cf739bff213b9c331db2c
Сергей <78658751+yolmane@users.noreply.github.com>
2022-12-27 22:07:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
 #include<stdio.h>
+ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main.cpp
 main()
 {
-    int n, numb, min, max, sum;    
-    printf("vedite kol-vo chisel virabotki: ");
-    scanf("%i",&n);
-    printf("vvedite pervoe chislo: ");
-    scanf("%i", &numb);
-    min = numb;
-    max = numb;
-    sum = numb;
-    for(int i = 0; i < n-1; i++)
+    int mas[3];
+    
+    for(int i=0;i<3;i++)
     {
-        printf("vvedite sleduyouwee chislo: ");
-        scanf("%i", &numb);
-        if (min > numb)
-        {
-            min = numb;
-        }
-        if (max < numb)
-        {
-            max = numb;
-        }
-        sum = sum + numb;
-    }    
-    printf("min=%i\nmax=%i\nsum=%i\navg=%f\n", min, max, sum, ((float)sum / (float)n));
-
+    	scanf("%i",&mas[i]);
+	}
+    
+    for(int i=0;i<3;i++)
+    {
+    	 printf("%i ",mas[i]);
+	}
 }
