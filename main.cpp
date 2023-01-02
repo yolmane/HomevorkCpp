eb7793faf8e8c340a738a6ee64e1a0b15b080a8e
Сергей <78658751+yolmane@users.noreply.github.com>
2023-01-02 18:23:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
-ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main.cpp
+//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main.cpp
 main()
 {
-    int mas[3];
+    int mas[7];
     
-    for(int i=0;i<3;i++)
+    for(int i=0;i<7;i++)
     {
     	scanf("%i",&mas[i]);
 	}
     
-    for(int i=0;i<3;i++)
+    for(int i=0;i<7;i++)
     {
     	 printf("%i ",mas[i]);
 	}
