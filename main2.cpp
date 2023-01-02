91a3151563da21842c1a3aafd69c707a292b4af1
Сергей <78658751+yolmane@users.noreply.github.com>
2023-01-02 18:22:37 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main2.cpp
+//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main2.cpp
 main()
 {
 	int n;
