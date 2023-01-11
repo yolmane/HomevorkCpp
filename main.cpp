e3506b67ce568b37dccb8e1ac2a296ff2552fcbf
Сергей <78658751+yolmane@users.noreply.github.com>
2023-01-12 00:24:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,46 @@
 #include<stdio.h>
-//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/lesson4.2/main.cpp
+//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/Lesson4.3/main.cpp
 main()
 {
-    int mas[7];
+    int mas[100];
+    int n;
     
-    for(int i=0;i<7;i++)
-    {
-    	scanf("%i",&mas[i]);
-	}
+    printf("kol el");
+    scanf("%i",&n);
     
-    for(int i=0;i<7;i++)
+    for(int i=0;i<n;i++)
     {
-    	 printf("%i ",mas[i]);
+    	scanf("%4i",&mas[i]);
 	}
-}
+	
+	for(int i=0;i<n;i++)
+    {
+    	printf("%4i",mas[i]);
+	}
+	printf("\n");
+	
+	bool fl=true;
+	
+	int ch=0;
+	while(fl)
+	{
+		fl=false;
+		for(int i=0;i<n-1;i++)
+		{
+			if(mas[i]>mas[i+1])
+			{
+				int z=mas[i];
+				mas[i]=mas[i+1];
+				mas[i+1]=z;
+				fl=true;
+			}
+		}
+		ch++;
+	}
+	
+	for(int i=0;i<n;i++)
+    {
+    	printf("%4i",mas[i]);
+	}
+	printf("\n %i",ch);
+} 
