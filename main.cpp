54254a004e5c22d7f7ffaee0d0d1946c32a17983
Сергей <78658751+yolmane@users.noreply.github.com>
2023-01-15 10:44:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,36 +4,27 @@ main()
 {
     int mas[100];
     int n;
-    
     printf("el mas ");
     scanf("%i",&n);
-    
     for(int i=0;i<n;i++)
     {
     	scanf("%i",&mas[i]);
-	}
-	
+    }
 	int min,max;
 	min=max=mas[0];
-	
 	int sum=0;
-	
 	for(int i=0;i<n;i++)
 	{
 		if(mas[i]>max)
 		{
 			max=mas[i];
 		}
-		
 		if(mas[i]<min)
 		{
 			min=mas[i];
 		}
-		
 		sum+=mas[i];
 	}
-	
 	float sr=(float)sum/(float)n;
-	
 	printf("Min=%i Max=%i Sum=%i Sr=%f",min,max,sum,sr);
 }
