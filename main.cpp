8e8bb323e5c6e209de850c18798cbd97dd9dfd70
Сергей <78658751+yolmane@users.noreply.github.com>
2023-02-04 00:25:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include<stdio.h>
-//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/Lesson4.4/main.cpp
+//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/Lesson5.2/main.cpp
 main()
 {
-    int mas[100];
-    int n;
-    printf("el mas ");
-    scanf("%i",&n);
-    for(int i=0;i<n;i++)
-    {
-    	scanf("%i",&mas[i]);
-    }
-    int min,max;
-    min=max=mas[0];
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-	if(mas[i]>max)
+	int mas[100][100];
+	int n,m;
+	printf("n= ");
+	scanf("%d",&n);
+	printf("m= ");
+	scanf("%d",&m);
+	for(int j=0;j<n;j++)
 	{
-	    max=mas[i];
+		for(int i=0;i<m;i++)
+		{
+			scanf("%i",&mas[j][i]);
+		}
 	}
-	if(mas[i]<min)
+	for(int j=0;j<n;j++)
 	{
-	    min=mas[i];
+		for(int i=0;i<m;i++)
+		{
+			printf("%3i",mas[j][i]);
+		}
+		printf("\n\n");
 	}
-	sum+=mas[i];
-    }
-    float sr=(float)sum/(float)n;
-    printf("Min=%i Max=%i Sum=%i Sr=%f",min,max,sum,sr);
 }
