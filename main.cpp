3211f165d06b9af682dacb0497a78978ae3f0b25
Сергей <78658751+yolmane@users.noreply.github.com>
2023-02-18 15:27:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
-//ссыль на гит  https://github.com/yolmane/HomevorkCpp/tree/Lesson5.5/main.cpp
+
 main()
 {
     int mas[100][100];
@@ -10,16 +9,16 @@ main()
     
     while ((n < 1) || (n > 100))
     {
-        printf("kol-vo strok 1-100 ");
+        printf("Ведите колличество строк массива от 1 до 100 включительно ");
         scanf("%i",&n);
     }
     while ((m < 1) || (m > 100))
     {
-        printf("kol-vo stolbcov 1-100 ");
+        printf("Ведите колличество строк массива от 1 до 100 включительно ");
         scanf("%i",&m);
     }
     
-    printf("vvodite el. massiva cherez ENTER \n");
+    printf("Вводите значения двумерного массива построчно через Enter \n");
     
     for (int j=0; j<n; j++)
     {
@@ -28,8 +27,38 @@ main()
             scanf("%i",&mas[j][i]);
         }
     }
-   
-    printf("\nVvedenniy massiv \n\n");
+
+    printf("\nИсходный массив\n\n");
+
+    for (int j=0; j<n; j++)
+    {
+        for(int i=0; i<m; i++)
+        {
+            printf("%3i",mas[j][i]);
+        }
+        printf("\n");
+    }    
+
+    printf("\nMассив после сортировки построчно\n\n");
+    bool fl;
+    for(int i=0;i<n;i++)
+    {
+        fl = true;
+        while(fl)
+        {
+            fl = false;
+            for(int j=0;j<m-1;j++) 
+            {
+                if(mas[i][j] > mas[i][j+1])
+                {
+                    int q=mas[i][j];
+                    mas[i][j] = mas[i][j+1];
+                    mas[i][j+1] = q;
+                    fl = true;
+                }
+            }
+        }
+    }
 
     for (int j=0; j<n; j++)
     {
@@ -39,33 +68,4 @@ main()
         }
         printf("\n");
     }
-
-    printf("\n");
-
-    int k = m;
-    if(n > k)
-    {
-        k = n;
-    }
-    int mas2[100][100];
-
-    for (int j=0; j<k; j++)
-    {
-        for(int i=0; i<k; i++)
-        {
-            mas2[j][i]=mas[i][j];
-        }
-    }
-    
-    printf("Massiv posle inversii po glavnoy diagonale\n\n");
-
-    for (int j=0; j<m; j++)
-    {
-        for(int i=0; i<n; i++)
-        {
-            printf("%3i",mas2[j][i]);
-        }
-        printf("\n");
-    }
-    
 }
