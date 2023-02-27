cc86440ffc142f5284b1ea8de227efe8d932d49a
Сергей <78658751+yolmane@users.noreply.github.com>
2023-02-27 23:00:03 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+//  https://github.com/yolmane/HomevorkCpp/tree/Lesson6/3.cpp
+main()
+{
+	char str[100];
+	gets(str);
+	
+	int i;
+	printf("sumb=");
+	scanf("%i",&i);
+	for(int j=i-1;j<strlen(str);j++)
+	{
+		str[j]=str[j+1];
+	}
+	puts(str);
+}
